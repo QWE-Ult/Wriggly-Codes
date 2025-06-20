050a737baf3fcbc090ddb4361fc33b3aec3f6d3a
Yash Meghnathi <yashmeghnathi07@gmail.com>
2025-06-21 02:57:31 +0530

    Delete zip (1) directory

--- zip (1)/contracts/Codes(EASY)/Simple Hash Two.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.8.0;
-
-contract Solution {
-    function ripemdHash(string memory _message) public pure returns (bytes20) {
-        // TODO
-     return ripemd160(abi.encodePacked(_message));
-    }
-}
--- zip (1)/contracts/Codes(EASY)/String Length.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0;
-
-contract Sol {
-    function getStringLength(string memory str) public pure returns (uint) {
-        return bytes(str).length;
-    }
-}
